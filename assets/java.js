a33edf8e47fd28c40b4fb56fe663e92507b507ed
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-06-09 16:05:24 -0500

    favicon added

--- assets/java.js
+++ assets/java.js
@@ -9,157 +9,140 @@ var firebaseConfig = {
     messagingSenderId: "410370824239",
     appId: "1:410370824239:web:0b19bdff052fe0d4"
   };
-  // Initialize Firebase
+// Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 // creates a variable to reference the database
   var database = firebase.database();
 
+  
 // Capturing the button click
  $("#addTrain").on("click", function(){
 
-// preventing multiple submission after pressing the button
-event.preventDefault();
+     // preventing multiple submission after pressing the button
+     event.preventDefault();
 
-// Initial Variables & grabbing values from text-boxes
+     // Initial Variables & grabbing values from text-boxes
      var trainName = $("#trainName").val().trim();
-     var desination = $("#desination").val().trim();
+     var destination = $("#destination").val().trim();
      var firstTrainTime = $("#firstTrainTime").val().trim();
      var frequency = $("#frequency").val().trim();
-     
-     
-    var newTrain = {
-        trainName: trainName,
-        desination: desination,
-        firstTrainTime: firstTrainTime,
-        frequency: frequency
-        
-    }
+
+     //  if the text boxes are not filled in, the user will be notified
+         if (!trainName || !destination || !firstTrainTime || !frequency){
+         alert("Please fill in all text boxes")
+         }
+     // if not a number is added to frequency, the user will be notified
+         else if (isNaN(frequency)){
+         alert("Please type a valid number")
+         }
+         else{
+            var newTrain = {
+            trainName: trainName,
+            destination: destination,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency
+            }
+         }   
 // pushes information to firbase
-    database.ref().push(newTrain);
-
-    // alert - user that info is input
-    // clear boxes of input form
-// append info to table
-
+     database.ref().push(newTrain);
+    
+    //  clears textbox after form submitted
+     function clearText() {
+        $("textarea").val("");
+     }
+     
+     clearText()
  });
 
 
+ //  Materialize JS
+ $('#trainName').val();
+ M.textareaAutoResize($('#trainName'));
 
+ $('#destination').val();
+ M.textareaAutoResize($('#destination'));
 
+ $('#frequency').val();
+ M.textareaAutoResize($('#frequency'));
 
-database.ref().on("child_added", function(snapshot){
-    // get the value from the snapshot
-    var sv = snapshot.val();
+ $('#firstTrainTime').val();
+ M.textareaAutoResize($('#firstTrainTime'));
 
-    console.log('sv', sv);
+ 
+ database.ref().on("child_added", function(snapshot){
     
-
-    // console.log(sv.trainName);
-    // console.log(sv.desination);
-    // console.log(sv.firstTrainTime);
-    // console.log(sv.frequency);
+    // get the value from the snapshot
+     var sv = snapshot.val();
 
     // Changing HTML to reflect new data
-var trainName = snapshot.val().trainName;
-var desination = snapshot.val().desination;
-var firstTrainTime = snapshot.val().firstTrainTime;
-var frequency = snapshot.val().frequency;
+     var trainName = snapshot.val().trainName;
+     var destination = snapshot.val().destination;
+     var frequency = snapshot.val().frequency;
 
-$("#table-name").text(trainName);
-$("#table-destination").text(desination);
-$("#table-frequency").text(frequency);
-    //jquery to append to you html table
+    //jquery to append to your html table
+     var tr=$("<tr>");
 
-var tr=$("<tr>");
+     var td1=$("<td>");
+     td1.text(trainName)
+     tr.append(td1)
 
-var td1=$("<td>");
-td1.text(trainName)
-tr.append(td1)
+     var td2=$("<td>");
+     td2.text(destination)
+     tr.append(td2)
 
-var td2=$("<td>");
-td2.text(desination)
-tr.append(td2)
-
-var td3=$("<td>");
-td3.text(frequency)
-tr.append(td3)
-
-
-var tFrequency = sv.frequency;
-
-var firstTime = sv.firstTrainTime;
-
-var firstTimeConverted = moment(firstTime, "HH:mm")
-console.log(firstTimeConverted);
-
-var currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
-
-var tRemainder = diffTime % tFrequency;
-console.log(tRemainder);
-
-
-var tMinutesTillTrain = tFrequency - tRemainder;
-console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-//starttime
-//frequency
+     var td3=$("<td>");
+     td3.text(frequency)
+     tr.append(td3)
 
 
 
+     var tFrequency = sv.frequency;
 
-var td4=$("<td>");
-td4.text(moment(nextTrain).format("hh:mm"))
-tr.append(td4)
+     var firstTime = sv.firstTrainTime;
 
-var td5=$("<td>");
-td5.text(tMinutesTillTrain)
-tr.append(td5)
+     var firstTimeConverted = moment(firstTime, "HH:mm")
 
+     var currentTime = moment();
+     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-$("tbody").append(tr);
+     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-// handles errors
-}, function(errorObject) {
-    console.log("Errors Handled: " + errorObject.code);
-});
-// Assumption
-var tFrequency = 10;
+     if(diffTime < 0){
+        diffTime = diffTime * -1 
+     }
 
-var firstTime = "14:50";
+     var tRemainder = diffTime % tFrequency;
 
-var firstTimeConverted = moment(firstTime, "HH:mm")
-console.log(firstTimeConverted);
+     var tMinutesTillTrain = tFrequency - tRemainder;
 
-var currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
-
-var tRemainder = diffTime % tFrequency;
-console.log(tRemainder);
+     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
 
-var tMinutesTillTrain = tFrequency - tRemainder;
-console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+     var td4=$("<td>")
+     td4.text(moment(nextTrain).format("HH:mm"))
+     tr.append(td4)
 
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-// bonus - refresh page every minute to update table - reference table or page
-// moment.js library - understand how to read the minutes and calcutate how many minutes between
+     var td5=$("<td>");
+     td5.text(tMinutesTillTrain)
+     tr.append(td5)
 
-//  what is happening
-// be added to the table - dynamic rows with the information
-// create a firebas listener
-//     // need mooment.js to calculate the next train and the remaind minutes
+
+     $("tbody").append(tr);
+
+
+        // handles errors
+     }, function(errorObject) {
+     console.log("Errors Handled: " + errorObject.code);
+     });
+ // assuming train stop at midnight (last train)
+ // if (diffTime < 0){
+ //     nextTrain = firstTime
+ //     diffTime = diffTime * -1 
+ // }
+
+//  https://itsolutionstuff.com/post/automatically-refresh-or-reload-a-page-using-jquery-exampleexample.html
+   setTimeout(function(){
+       location.reload();
+   },60000);
 
 });
-// clean up logs, maybe look into adding 
-// edge case - prevent user from adding non numbers 
-// if to clean adding future time
